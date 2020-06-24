008daeffe14112e497def00c9cfddf87db051456
headsht.100.beatch <headsht.100.beatch@mail.ru>
2020-06-24 23:39:33 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace зачет_
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+             Console.WriteLine("Введите название документа");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+               
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    Console.WriteLine(await sr.ReadToEndAsync());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
